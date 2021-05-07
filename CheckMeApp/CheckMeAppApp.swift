6d796a58a876d7d6982e19daf3dead7e8aeeda4b
Rafinad <rafinad@outlook.com>
2021-05-07 14:06:46 +0300

    Initial Commit

---
+++ CheckMeApp/CheckMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckMeAppApp.swift
+//  CheckMeApp
+//
+//  Created by Сахар Кубический on 07.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CheckMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
